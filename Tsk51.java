7b00ee5e1c1b5f6b23c2c587a7fb0a42277f32d2
НЁРФИК <154437363+nerffFE@users.noreply.github.com>
2024-12-21 10:59:04 +0300

    Update Tsk51.java

--- Tsk51.java
+++ Tsk51.java
@@ -1,7 +1,8 @@
 package Task5;
 
 public class Tsk51 {
-    //https://education.yandex.ru/ege/task/2e09b9f5-1927-41a3-8963-2ea21f82a52b
+    // Задача на системы счисления
+    // тут: пятиричная
     public static void main(String[] args) {
         for (int i = 0; i < 10000; i++) {
             if (F(i) == 61)
